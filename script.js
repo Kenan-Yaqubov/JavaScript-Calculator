9263f035d9ca673dbe6f0299303e667996611b71
YaqubovKenan <yagubov.k.c@gmail.com>
2024-03-20 17:47:46 +0400

    Calculator Modified

--- script.js
+++ script.js
@@ -5,21 +5,34 @@ let sign;
 let answer;
 
 function press(btn) {
+    if (input.value == 0) {
+        input.value = "";
+    }
     if (input.value.length > 9) {
         alert("It is maximum characters!");
     } else {
-        if (btn.innerText === '+' || btn.innerText === '-' || btn.innerText === '*' || btn.innerText === '/') {
+        if (btn.innerText === '+' || btn.innerText === '-' || btn.innerText === '*' || btn.innerText === '/' || btn.innerText === '%' || btn.innerText === '^') {
             input.value = "";
             sign = btn.innerText;
-            input.value += btn.innerText;
-        } else if (input.value === '+' || input.value === '-' || input.value === '*' || input.value === '/') {
+            input.value += btn.innerText
+        } else if (input.value === '+' || input.value === '-' || input.value === '*' || input.value === '/' || input.value === '%' || input.value === '^') {
             input.value = "";
-            input.value += btn.innerText;
+            if (btn.innerText === 'AC') {
+                sign = undefined;
+                numArr = []
+                input.value = 0;
+            } else if (btn.innerText === 'C') {
+                input.value = numArr[0];
+                sign = undefined;
+            } else {
+                input.value += btn.innerText;
+            }
             numArr[1] = input.value;
         } else if (btn.innerText === '=') {
             if (numArr[1] === undefined) {
                 input.value = input.value;
             } else {
+                input.value = "";
                 const num1 = Number(numArr[0]);
                 const num2 = Number(numArr[1]);
                 console.log(num1 + num2);
@@ -36,10 +49,31 @@ function press(btn) {
                         return;
                     }
                     answer = num1 / num2;
+                } else if (sign === '%') {
+                    answer = (num1 / num2) * 100
+                } else if (sign === '^') {
+                    answer = num1 ** num2
                 }
                 input.value = answer;
                 numArr = [];
                 numArr.push(answer.toString());
+            }
+        } else if (btn.innerText === 'AC') {
+            sign = undefined;
+            numArr = []
+            input.value = 0;
+        } else if (btn.innerText === 'C') {
+            if (sign === undefined) {
+                input.value = input.value.slice(0, -1);
+                numArr[0] = input.value;
+            } else {
+                input.value = input.value.slice(0, -1);
+                numArr[1] = input.value;
+            }
+
+            if (input.value == sign) {
+                input.value = numArr[0];
+                sign = undefined;
             }
         } else {
             input.value += btn.innerText;
