78784031ef24c174c796e548e736df27f994a3c5
YaqubovKenan <yagubov.k.c@gmail.com>
2024-03-21 11:40:13 +0400

    Calculator modified

--- script.js
+++ script.js
@@ -2,86 +2,109 @@ let input = document.getElementById('screen');
 let darkModeBtn = document.getElementById('dark')
 let numArr = [];
 let sign;
-let answer;
+let answer = 1;
 
 function press(btn) {
     if (input.value == 0) {
         input.value = "";
     }
-    if (input.value.length > 9) {
-        alert("It is maximum characters!");
-    } else {
-        if (btn.innerText === '+' || btn.innerText === '-' || btn.innerText === '*' || btn.innerText === '/' || btn.innerText === '%' || btn.innerText === '^') {
-            input.value = "";
-            sign = btn.innerText;
+    if (btn.innerText === '+' || btn.innerText === '-' || btn.innerText === '*' || btn.innerText === '/' || btn.innerText === '%' || btn.innerText === '^' || btn.innerText === '1/x' || btn.innerText === 'n!' || btn.innerText === '√') {
+        input.value = "";
+        sign = btn.innerText;
+        if (sign === '1/x') {
+            input.value = `1 / ${numArr[0]}`
+        } else if (sign === 'n!') {
+            input.value = `${numArr[0]}!`
+        } else if (sign === '√') {
+            input.value = `√${numArr[0]}`
+        } else {
             input.value += btn.innerText
-        } else if (input.value === '+' || input.value === '-' || input.value === '*' || input.value === '/' || input.value === '%' || input.value === '^') {
-            input.value = "";
-            if (btn.innerText === 'AC') {
-                sign = undefined;
-                numArr = []
-                input.value = 0;
-            } else if (btn.innerText === 'C') {
-                input.value = numArr[0];
-                sign = undefined;
-            } else {
-                input.value += btn.innerText;
-            }
-            numArr[1] = input.value;
-        } else if (btn.innerText === '=') {
-            if (numArr[1] === undefined) {
-                input.value = input.value;
-            } else {
-                input.value = "";
-                const num1 = Number(numArr[0]);
-                const num2 = Number(numArr[1]);
-                console.log(num1 + num2);
-                console.log(sign);
-                if (sign === '+') {
-                    answer = num1 + num2;
-                } else if (sign === '-') {
-                    answer = num1 - num2;
-                } else if (sign === '*') {
-                    answer = num1 * num2;
-                } else if (sign === '/') {
-                    if (num2 === 0) {
-                        alert("Division by zero!");
-                        return;
-                    }
-                    answer = num1 / num2;
-                } else if (sign === '%') {
-                    answer = (num1 / num2) * 100
-                } else if (sign === '^') {
-                    answer = num1 ** num2
-                }
-                input.value = answer;
-                numArr = [];
-                numArr.push(answer.toString());
-            }
-        } else if (btn.innerText === 'AC') {
+        }
+    } else if (input.value === '+' || input.value === '-' || input.value === '*' || input.value === '/' || input.value === '%' || input.value === '^') {
+        input.value = "";
+        if (btn.innerText === 'AC') {
             sign = undefined;
             numArr = []
             input.value = 0;
-        } else if (btn.innerText === 'C') {
-            if (sign === undefined) {
-                input.value = input.value.slice(0, -1);
-                numArr[0] = input.value;
-            } else {
-                input.value = input.value.slice(0, -1);
-                numArr[1] = input.value;
-            }
-
-            if (input.value == sign) {
-                input.value = numArr[0];
-                sign = undefined;
-            }
+        } else if (btn.innerText === 'C' || btn.innerText === 'CE') {
+            input.value = numArr[0];
+            sign = undefined;
         } else {
             input.value += btn.innerText;
-            if (sign === undefined) {
-                numArr[0] = input.value;
-            } else {
-                numArr[1] = input.value;
+        }
+        numArr[1] = input.value;
+    } else if (btn.innerText === '=') {
+        if (numArr[1] === undefined && sign != '1/x' && sign != 'n!' && sign != '√') {
+            input.value = input.value;
+        } else {
+            input.value = "";
+            const num1 = Number(numArr[0]);
+            const num2 = Number(numArr[1]);
+            console.log(num1 + num2);
+            console.log(sign);
+            if (sign === '+') {
+                answer = num1 + num2;
+            } else if (sign === '-') {
+                answer = num1 - num2;
+            } else if (sign === '*') {
+                answer = num1 * num2;
+            } else if (sign === '/') {
+                if (num2 === 0) {
+                    alert("Division by zero!");
+                    return;
+                }
+                answer = num1 / num2;
+            } else if (sign === '%') {
+                answer = (num1 / num2) * 100
+            } else if (sign === '^') {
+                answer = num1 ** num2
+            } else if (sign === '1/x') {
+                answer = 1 / num1
+            } else if (sign === 'n!') {
+                for (let i = 1; i <= num1; i++) {
+                    answer *= i;
+                }
+            } else if (sign === '√'){
+                answer = Math.sqrt(num1)
             }
+            input.value = answer;
+            answer = 1;
+            numArr = [];
+            numArr.push(answer.toString());
+        }
+    } else if (btn.innerText === 'AC') {
+        sign = undefined;
+        numArr = []
+        input.value = 0;
+    } else if (btn.innerText === 'C') {
+        if (sign === undefined) {
+            input.value = input.value.slice(0, -1);
+            numArr[0] = input.value;
+        } else {
+            input.value = input.value.slice(0, -1);
+            numArr[1] = input.value;
+        }
+    } else if (btn.innerText === 'CE') {
+        if (sign === undefined) {
+            input.value = 0;
+            numArr[0] = input.value;
+        } else {
+            input.value = 0;
+            numArr[1] = input.value;
+        }
+    } else if (btn.innerText === 'π') {
+        input.value += btn.innerText;
+        if (sign === undefined) {
+            numArr[0] = 3.14159265359;
+        } else {
+            numArr[1] = 3.14159265359;
+        }
+    } else {
+        input.value += btn.innerText;
+        if (sign === '+' || sign === '-' || sign === '/' || sign === '*' || sign === '%' || sign === '^') {
+            numArr[1] = input.value;
+        } else {
+            numArr[0] = input.value;
         }
     }
 }
